ac31f5485023694eb27ff36a088e7e3e59288bc7
Harshit Gulati <harshit.gulati.999@outlook.com>
2025-10-02 17:23:03 +0530

    added favicon

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
